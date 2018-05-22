f2182ce5efb81ba9e3a242e2928e946df16d1e78
Adrian de la Rosa <adrian@delarosab.me>
2018-05-22 10:57:17 +0200

    🚑 Fixing bootstrap

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const app = require('./app');
 
 app
   .use('/items', require('./controllers/items'))
-  .use('/taxonomies', require('./controllers/taxonomies'))
   .all('*', (req, res) => res.sendStatus(404))
   .listen(3000);
