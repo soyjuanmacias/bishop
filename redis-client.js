f712cb6eb622fbf72dffcd55fe249819816ec6b5
Juan Macías Opinno <juan.macias@opinno.com>
2019-01-21 14:43:15 +0100

    feat(Redis-Client): now the cache is hot when server start

--- redis-client.js
+++ redis-client.js
@@ -7,5 +7,14 @@ module.exports = {
   ...client,
   getAsync: promisify(client.get).bind(client),
   setAsync: promisify(client.set).bind(client),
-  keysAsync: promisify(client.keys).bind(client)
+  keysAsync: promisify(client.keys).bind(client),
+  existsAsync: promisify(client.exists).bind(client),
+  deleteAllAsync: promisify(client.flushall).bind(client)
 };
+
+const Gateway = require('./services/gateway.service');
+
+// Let's hot the cache when server start
+client.on("connect", () => {
+  Gateway.loadDataAndCache();
+});
